b5746df452db19e0285ebb9ff948204ad7ad44a2
Khoa <khoavodang1451997@gmail.com>
2024-03-17 14:30:29 +0700

    add secret key base

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+end
